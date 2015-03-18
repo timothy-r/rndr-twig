583cd7626ae2911146c7d8fcba8962092ba8e172
Tim Rodger <tim.rodger@gmail.com>
2015-03-18 01:26:06 +0000

    Updates code comments

--- src/app.php
+++ src/app.php
@@ -24,19 +24,22 @@ $app->get('/', function () use ($app) {
  */
 $app->post("{path}", function(Request $req, $path) use ($app, $twig){
 
-    // use request content-type to convert request into an array
+    // Should use request content-type to convert request into an array and not assume json
     // there's a lib for this in php, right?
     $req_vars = json_decode($req->getContent(), 1);
 
     try {
         // try to find the template
         // render with var in body of request
-        // respond with rendered result
         $result = $twig->loadTemplate($path . '.twig')->render($req_vars);
+        // Ought to try to figure out the response content type
+        // respond with rendered result
         return new Response($result, 200);
     } catch (Exception $ex) {
         return new Response('', 404);
     }
 });
+
+// add router for other request methods to reject them
 
 return $app;
