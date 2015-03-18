3fd8c8517142c074453c273ec04f924d559b0d57
Tim Rodger <tim.rodger@gmail.com>
2015-03-18 22:04:30 +0000

    Minor tweaks

--- src/app.php
+++ src/app.php
@@ -30,7 +30,6 @@ $app->get('/', function () use ($app) {
  * Catch all post requests to any path
  */
 $app->post("{path}", function(Request $req, $path) use ($app){
-
     // Should use request content-type to convert request into an array and not assume json
     // there's a lib for this in php, right?
     $req_vars = json_decode($req->getContent(), 1);
@@ -39,7 +38,6 @@ $app->post("{path}", function(Request $req, $path) use ($app){
         // try to find the template
         $result = $app['twig']->loadTemplate($path . '.twig')->render($req_vars);
         // Ought to try to figure out the response content type
-        // respond with rendered result
         return new Response($result, 200);
     } catch (Exception $ex) {
         return new Response($ex->getMessage(), 404);
@@ -48,7 +46,7 @@ $app->post("{path}", function(Request $req, $path) use ($app){
 
 $app->error(function (Exception $e, $code) use($logger) {
     $logger->addError($e->getMessage());
-    return new Response($e->getMessage(), 404, ['Content-Type' => 'text/plain']);
+    return new Response($e->getMessage(), $code, ['Content-Type' => 'text/plain']);
 });
 
 return $app;
