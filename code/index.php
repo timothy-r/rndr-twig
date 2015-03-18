2843846a39756f66fced47f0324408fa81ca87dd
Tim Rodger <tim.rodger@gmail.com>
2015-03-18 00:13:57 +0000

    Updates routes

--- code/index.php
+++ code/index.php
@@ -1,11 +1,29 @@
 <?php
 
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+$app = new Application();
 
 $app->get('/', function () use ($app) {
-    return '';
+    return new Response(json_encode(
+        ['name' => 'render', 'desc' => 'Renders templates']),
+        200,
+        ['Content-Type' => 'application/json']
+    );
+});
+
+/**
+ * Catch all post requests to any path
+ */
+$app->post("{path}", function(Request $req, $path) use ($app){
+   // try to find the template
+   // render with var in body of request (use Content-Type to figure out how to decode body)
+   // respond with rendered result
+
 });
 
 $app->run();
