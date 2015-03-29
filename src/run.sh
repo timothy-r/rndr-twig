b38b65a6aee5d6b1dadcf45fc2a6492cfe78fef7
Tim Rodger <tim.rodger@gmail.com>
2015-03-29 16:45:38 +0100

    Adds comments to run script

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo fastcgi_param REDIS_PORT $REDIS_PORT >> /etc/nginx/fastcgi_params
+# inject the REDIS_PORT env var into redis config
+
+echo fastcgi_param REDIS_PORT $REDIS_PORT; >> /etc/nginx/fastcgi_params
 
 # Start the server
 supervisord --nodaemon
