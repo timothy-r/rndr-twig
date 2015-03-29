ea7da05334c28f0d3d3d5e2001cb9d9714e0b1fa
Tim Rodger <tim.rodger@gmail.com>
2015-03-29 16:57:49 +0100

    Updates writing to nginx config

--- src/run.sh
+++ src/run.sh
@@ -2,7 +2,7 @@
 
 # inject the REDIS_PORT env var into redis config
 
-echo fastcgi_param REDIS_PORT $REDIS_PORT; >> /etc/nginx/fastcgi_params
+echo "fastcgi_param REDIS_PORT $REDIS_PORT;" >> /etc/nginx/fastcgi_params
 
 # Start the server
 supervisord --nodaemon
