ce4623e83dacca22facf1fa86f5bfdf72ecd1567
Tim Rodger <tim.rodger@gmail.com>
2015-09-01 20:53:29 +0100

    Configure monolog.name property

--- src/app.php
+++ src/app.php
@@ -14,8 +14,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 $app = new Application();
 
 $app->register(new ConfigProvider());
-$app['monolog.logfile'] = "php://stdout";
+
 $app->register(new MonologServiceProvider());
+$app['monolog.logfile'] = "php://stdout";
+$app['monolog.name'] = 'render';
+
 $app->register(new ErrorHandlerProvider());
 
 $app->register(new StoreProvider());
