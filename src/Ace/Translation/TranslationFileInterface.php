0a5ee00a8a57cdb3af74398bb54ee6a77e259530
Tim Rodger <tim.rodger@gmail.com>
2015-11-15 10:51:19 +0000

    Updates the interface for translation files

---
+++ src/Ace/Translation/TranslationFileInterface.php
@@ -0,0 +1,22 @@
+<?php namespace Ace\Translation; 
+/**
+ * @author timrodger
+ * Date: 30/10/15
+ */
+class TranslationFileInterface
+{
+    public function add($language, $file)
+    {
+
+    }
+
+    public function listLanguages()
+    {
+
+    }
+
+    public function getFile($language)
+    {
+
+    }
+}
